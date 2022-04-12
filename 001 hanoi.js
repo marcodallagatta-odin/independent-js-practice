ef67d4c8a48fcf84c5713bc8d0424592c2965d70
Marco Dalla Gatta <36917918+marcodallagatta@users.noreply.github.com>
2022-04-12 13:36:57 +0200

    add print function and new tests

--- 001 hanoi.js
+++ 001 hanoi.js
@@ -1,34 +1,40 @@
 function hanoi(array) {
 	let locAr = array;
-
 	let i = 0;
 
-	console.log(i + ':' + locAr[0]);
-	console.log(i + ':' + locAr[1]);
-	console.log(i + ':' + locAr[2]);
-	console.log('---');
+	printHanoi(locAr, i);
 	i++;
 
 	while (locAr[0] != '') {
 		locAr[1] += locAr[0].slice(-1);
 		locAr[0] = locAr[0].slice(0,-1);
-		console.log(i + ':' + locAr[0]);
-		console.log(i + ':' + locAr[1]);
-		console.log(i + ':' + locAr[2]);
-		console.log('---');
+		printHanoi(locAr, i);
 		i++;
 	}
 
 	while (locAr[1] != '') {
 		locAr[2] += locAr[1].slice(-1);
 		locAr[1] = locAr[1].slice(0,-1);
-		console.log(i + ':' + locAr[0]);
-		console.log(i + ':' + locAr[1]);
-		console.log(i + ':' + locAr[2]);
-		console.log('---');
+		printHanoi(locAr, i);
 		i++;
 	}
 }
 
+function printHanoi(array, i) {
+	console.log(i + ':' + array[0]);
+	console.log(i + ':' + array[1]);
+	console.log(i + ':' + array[2]);
+	console.log('---');
+}
 
-hanoi(['ABC', '', ''])
+console.log('--- 1 BLOCK ---');
+hanoi(['A', '', '']);
+
+console.log('--- 3 BLOCKS ---');
+hanoi(['ABC', '', '']);
+
+console.log('--- 6 BLOCK ---');
+hanoi(['ABCDEF', '', '']);
+
+console.log('--- TO INFINITY AND BEYOND ---');
+hanoi(['ABCDEFGHILMNOPQRSTUVZ', '', '']);
