7c5295d29d17dabb42eccb23935166e3b9b4b354
Marco Dalla Gatta <36917918+marcodallagatta@users.noreply.github.com>
2022-04-12 14:08:57 +0200

    challenge 2: three highest number in string of numbers

---
+++ 002 third highest.js
@@ -0,0 +1,19 @@
+// write a program that reads ten numbers and
+// figures out which number is the third highest
+
+//psuedocode:
+// convert string to numbers in array;
+// sort them in descending order;
+// print first 3 indexes.
+
+function thirdHighest(numbers) {
+	let array = numbers.split(' ').map(Number);
+	console.log('original: ' + array);
+
+	array = array.sort(function(a, b){return b-a});
+	console.log('descending: ' + array);
+
+	console.log(`three highest: ${array[0]}, ${array[1]}, ${array[2]}`);
+}
+
+thirdHighest('20 3 12 23 18 270 9 546');
